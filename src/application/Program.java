06225c2158d281f61df238681b27e84b96bfaad3
vitorbite <vitorbitencourt20@gmail.com>
2026-02-17 17:36:40 -0300

    Adicionando a classe da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		Connection conn = null;
-		try {
-			conn = DB.getConnection();
-			System.out.println("OI");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			conn.close();
-		}
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 }
